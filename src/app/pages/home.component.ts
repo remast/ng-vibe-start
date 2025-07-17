d21a96c7c36c779769007b7ac34574d98d96a4fa
Linkedin User <linkedin@MGR2-RM313M.linkedin.biz>
2025-07-17 14:05:17 +0200

    Reset to base

--- src/app/pages/home.component.ts
+++ src/app/pages/home.component.ts
@@ -1,19 +1,11 @@
 import { Component, inject } from '@angular/core';
-import { SmoothieService } from '../services/smoothie.service';
-import { SmoothieCardComponent } from '../components/smoothie-card.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
-  imports: [SmoothieCardComponent]
+  imports: []
 })
 export class HomeComponent {
-  private readonly smoothieService = inject(SmoothieService);
-  
-  protected readonly currentSmoothie = this.smoothieService.getCurrentSmoothie();
-  
-  protected getNewSmoothie(): void {
-    this.smoothieService.getRandomSmoothie();
-  }
+
 } 
